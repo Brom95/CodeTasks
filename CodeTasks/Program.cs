a2108bff5bb4bffc51812444a0091e581977b365
warfa <warfa@DESKTOP-59S9V4F>
2021-11-29 21:02:17 +0300

    Добавьте файлы проекта.

---
+++ CodeTasks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System.Linq;
+
+Console.WriteLine(AlphabetPosition("The sunset sets at twelve o' clock."));
+
+string AlphabetPosition(string text)
+{
+    return string.Join(
+      " ",
+      text
+      .ToLower()
+      .Where(i => (i - 96) < 27 && (i - 96) > 0)
+      .Select(i => i - 96));
+}
