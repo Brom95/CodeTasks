d3ad23ad5ca25cb325e7dde395738adf730584ee
Brom95 <warfard34@gmail.com>
2021-12-02 17:44:45 +0300

    FindMissingLetter task solution added

---
+++ FindTheMissingLetter/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(FindMissingLetter(new char[]{'a', 'b', 'c', 'd', 'f'}));
+Console.WriteLine(FindMissingLetter(new char[] { 'O', 'Q', 'R', 'S' }));
+
+char FindMissingLetter(char[] array)
+{
+    var hs = new HashSet<char>(array);
+    char missingLetter = array[0];
+    while (!hs.Add(missingLetter))
+    {
+        missingLetter++;
+    }
+    return missingLetter;
+}
