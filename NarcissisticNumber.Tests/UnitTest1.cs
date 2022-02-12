54455230b4200f18d7d061fbc10e49641752413e
Brom95 <warfard34@gmail.com>
2022-02-12 17:30:58 +0300

    The shorter -- the better

--- NarcissisticNumber.Tests/UnitTest1.cs
+++ NarcissisticNumber.Tests/UnitTest1.cs
@@ -1,17 +1,13 @@
 using NUnit.Framework;
 
-namespace NarcisticNumber.Tests
+namespace NarcisticNumber.Tests;
+public class Tests
 {
-    public class Tests
-    {
 
-        [Test]
-        [TestCase(153, ExpectedResult = true)]
-        [TestCase(1938, ExpectedResult = false)]
-        public bool Test153(int input)
-        {
-            return Narcissistic.IsNarcissistic(input);
-        }
+    [Test]
+    [TestCase(153, ExpectedResult = true)]
+    [TestCase(1938, ExpectedResult = false)]
+    public bool Test153(int input) => Narcissistic.IsNarcissistic(input);
 
-    }
+
 }
