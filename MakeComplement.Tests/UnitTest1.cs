eb6d92cb833f63fe230de38dd4010a8d9db6f446
Brom95 <warfard34@gmail.com>
2022-02-12 17:31:39 +0300

    shorter

--- MakeComplement.Tests/UnitTest1.cs
+++ MakeComplement.Tests/UnitTest1.cs
@@ -1,16 +1,12 @@
 using NUnit.Framework;
 
-namespace MakeComplement.Tests
+namespace MakeComplement.Tests;
+public class Tests
 {
-    public class Tests
-    {
-        
-        [Test]
-        [TestCase("ATTGC", ExpectedResult = "TAACG")]
-        [TestCase("GTAT", ExpectedResult = "CATA")]
-        public string Test(string dna)
-        {
-            return Complementator.MakeComplement(dna);
-        }
-    }
+
+    [Test]
+    [TestCase("ATTGC", ExpectedResult = "TAACG")]
+    [TestCase("GTAT", ExpectedResult = "CATA")]
+    public string Test(string dna) => Complementator.MakeComplement(dna);
+
 }
